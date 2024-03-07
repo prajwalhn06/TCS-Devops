b413d5b24c660ea9e4ab5c662ca60532819ea43b
root <root@ip-172-31-23-225.ap-southeast-2.compute.internal>
2024-03-07 04:02:58 +0000

    added updated login1 file

--- login1.java
+++ login1.java
@@ -1,2 +1,12 @@
-Welcome to Devops
-  WE are working in TCS
+Public class login
+{
+	public static void main()
+	{
+		int i=0;
+		for(i=0;i<10;i++)
+		{
+			system.out.Println("Number: "+i);
+		}
+	}
+}
+	
